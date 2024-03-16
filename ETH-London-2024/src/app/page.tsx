85cc5e07ec4fbc684a99a0d9da19fba838068f1f
G-Yes95 <g-yes95@protonmail.com>
2024-03-16 20:57:38 +0000

    added PDF box

--- ETH-London-2024/src/app/page.tsx
+++ ETH-London-2024/src/app/page.tsx
@@ -17,6 +17,7 @@ function App() {
   const { disconnect } = useDisconnect();
 
   const [textFields, setTextFields] = useState<TextFieldData[]>([{ value: '', status: 'Status : Unsigned' }]);
+  const [pdfUrl, setPdfUrl] = useState<string>('https://docamatic.s3.eu-west-1.amazonaws.com/prod/4b969005-6bc9-4da2-8525-18e4e1e017ec/4fea55cb-6abd-4b0d-906d-97835d7eda2f.pdf');
 
   const handleTextFieldChange = (index: number, value: string) => {
     const updatedTextFields = [...textFields];
@@ -31,13 +32,15 @@ function App() {
       console.log(url, "url");
       const response = await fetch(url);
 
-
       if (!response.ok) {
         throw new Error('Failed to fetch data');
       }
 
       const data = await response.json();
       console.log(data); // Handle response data here
+
+      // Assuming the response data contains the URL of the PDF
+      setPdfUrl(data.pdfUrl);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
@@ -100,8 +103,15 @@ function App() {
               Make REST API Call
             </button>
           </div>
+
+          {/* Box to embed PDF */}
+          {pdfUrl && (
+            <Box mt={2} height={'12vh'}>
+              <embed src={pdfUrl} type="application/pdf" width="100%" height="600px" />
+            </Box>
+          )}
         </div>
-      </div>
+      </div >
     </>
   );
 }
