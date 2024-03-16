55cc6e76e0efe06c2b3c5ff7768680170294bc41
saidam90 <saida.murtazaliyeva@yahoo.com>
2024-03-16 18:20:34 +0000

    Fix css and add image

--- ETH-London-2024/src/app/page.tsx
+++ ETH-London-2024/src/app/page.tsx
@@ -18,18 +18,22 @@ function App() {
           <Title />
           <TextField />
 
-          <div>
+          <div className={styles.contentLeft}>
             <div>
-              <h2>Account</h2>
+              <div>{status}</div>
+              <div>{error?.message}</div>
               <div>
-                status: {account.status}
-                <br />
-                addresses: {JSON.stringify(account.addresses)}
-                <br />
-                chainId: {account.chainId}
+                <MintNft />
               </div>
             </div>
-
+            <h2>Account</h2>
+            <div>
+              status: {account.status}
+              <br />
+              addresses: {JSON.stringify(account.addresses)}
+              <br />
+              chainId: {account.chainId}
+            </div>
             {account.status === "connected" && (
               <button
                 className={styles.actionButton}
@@ -39,27 +43,20 @@ function App() {
                 Disconnect
               </button>
             )}
+          </div>
 
-            <div>
-              <h2>Connect</h2>
-              <div className={styles.connectButtons}>
-                {connectors.map((connector) => (
-                  <button
-                    className={styles.actionButton}
-                    key={connector.uid}
-                    onClick={() => connect({ connector })}
-                    type="button"
-                  >
-                    {connector.name}
-                  </button>
-                ))}
-              </div>
-              <div>{status}</div>
-              <div>{error?.message}</div>
-              <div>
-                {" "}
-                <MintNft />{" "}
-              </div>
+          <div>
+            <div className={styles.connectButtons}>
+              {connectors.map((connector) => (
+                <button
+                  className={styles.actionButton}
+                  key={connector.uid}
+                  onClick={() => connect({ connector })}
+                  type="button"
+                >
+                  {connector.name}
+                </button>
+              ))}
             </div>
           </div>
         </div>
