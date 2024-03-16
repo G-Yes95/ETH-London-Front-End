3c8e78232f00c63de123f1cf00436043a0d34ad8
G-Yes95 <g-yes95@protonmail.com>
2024-03-16 00:49:39 +0000

    update for sign message

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,66 @@
-// import { DynamicWidget } from "../lib/dynamic";
-
-// export default function Home() {
-//   return (
-//     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-//       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-//         <DynamicWidget />
-//       </div>
-//     </main>
-//   );
-// }
-"use client"
+"use client";
 import { useDynamicContext, DynamicWidget } from "@dynamic-labs/sdk-react-core";
+// import {
+//   useContractWrite,
+//   usePrepareContractWrite,
+//   useWaitForTransaction,
+// } from "wagmi";
+// import { FC } from "react";
 
 const Main = () => {
   const { primaryWallet } = useDynamicContext();
 
   const signMessage = async (primaryWallet: any) => {
-    if (!primaryWallet) return null;
-    else {
+    if (!primaryWallet) {
+      console.log("No wallets are connected");
+    } else {
       console.log(primaryWallet.address);
     }
-
-    const signer = await primaryWallet.connector.getSigner();
+    // console.log("primaryWallet.connector", primaryWallet.connector);
+    console.log("primaryWallet          ", primaryWallet);
+    const signer = await primaryWallet.connector.ethers.getSigner();
 
     return signer ? await signer.signMessage("example") : null;
   };
+
+  // const { config } = usePrepareContractWrite({
+  //   abi: [
+  //     {
+  //       inputs: [],
+  //       name: "mint",
+  //       outputs: [],
+  //       stateMutability: "nonpayable",
+  //       type: "function",
+  //     },
+  //   ] as const,
+  //   address: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  //   functionName: "mint",
+  // });
+
+  // const { data, write } = useContractWrite(config);
+
+  // const { isLoading, isSuccess } = useWaitForTransaction({
+  //   hash: data?.hash,
+  // });
 
   return (
     <div>
       <DynamicWidget />
       <button onClick={() => signMessage(primaryWallet)}>Sign Message</button>
+      <div>
+        {/* <p>Mint NFT</p>
+        <button disabled={!write || isLoading} onClick={() => write?.()}>
+          {isLoading ? "Minting..." : "Mint"}
+        </button>
+        {isSuccess && (
+          <div>
+            Successfully minted your NFT!
+            <div>
+              <a href={`https://etherscan.io/tx/${data?.hash}`}>Etherscan</a>
+            </div>
+          </div>
+        )} */}
+      </div>
     </div>
   );
 };
